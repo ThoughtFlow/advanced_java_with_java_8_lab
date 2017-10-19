9f85c8af8829c7bf0ea077de56dbc7cc609f977e
Nick <info@thoughtflow.ca>
2017-10-19 13:11:43 -0700

    Added new util class for lab 13

---
+++ lab13/Util.java
@@ -0,0 +1,15 @@
+package lab13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class Util {
+
+	public static BufferedReader getReader(String stringedUrl) throws IOException {
+		
+	       URL url = new URL(stringedUrl);
+		   return new BufferedReader(new InputStreamReader(url.openConnection().getInputStream()));
+	}
+}
